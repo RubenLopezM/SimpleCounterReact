c68e816693a930308b78cc6fe6bd49911285a1b2
RubenLopezM <rublopmag@hotmail.com>
2021-11-08 21:02:56 +0000

    Exercise with react

---
+++ src/js/component/Counter.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+
+const Counter = props => {
+	const [cont, setCont] = useState(0);
+	//const [start, setStart] = useState(start);
+	useEffect(() => {
+		const timer = setInterval(() => {
+			if (cont < 9) {
+				setCont(cont + 1);
+			} else {
+				setCont(0);
+			}
+		}, props.time);
+		return () => clearInterval(timer); //No es todo el rato el mismo intervalo, el clear intervalo va matando el intervalo
+	}, [cont]);
+
+	return (
+		<div className="container">
+			<section>
+				<span>{cont}</span>
+			</section>
+		</div>
+	);
+};
+
+Counter.propTypes = {
+	time: PropTypes.number
+};
+
+export default Counter;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,18 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import Counter from "./Counter.jsx";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<section>
+				<Counter time={100000000} />
+				<Counter time={10000000} />
+				<Counter time={1000000} />
+				<Counter time={100000} />
+				<Counter time={10000} />
+				<Counter time={1000} />
+			</section>
 		</div>
 	);
 };
